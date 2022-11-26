ff7b6b8e7a438b44c6b1f1f4da1007c83d28cac7
Naitik Lodha <69336744+naitik-lodha@users.noreply.github.com>
2022-11-26 10:28:44 +0530

    Beautified code

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const {
-    MongoClient
-} = require('mongodb');
-const booksRoute = require('./routes/books.js');
-const membersRoute = require('./routes/members.js')
-
+const { MongoClient } = require("mongodb");
+const booksRoute = require("./routes/books.js");
+const membersRoute = require("./routes/members.js");
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'library';
+const dbName = "library";
 var temp = "",
-    temp2 = ""
-
-
-
+  temp2 = "";
 
 //Default page
-app.get('/', (req, res) => {
-
-    res.send("Hello,Welcome to our Library please insert a route on search bar")
-
+app.get("/", (req, res) => {
+  res.send("Hello,Welcome to our Library please insert a route on search bar");
 });
 //Books page(List of all books)
-app.use('/books', booksRoute)
-
+app.use("/books", booksRoute);
 
 //Showing all the members in the database
-app.use('/members', membersRoute)
+app.use("/members", membersRoute);
 
-
-
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port  ${port}.. `));
